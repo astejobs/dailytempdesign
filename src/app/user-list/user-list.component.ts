86ae8cb219d056f29dfafbcbfeed931c51e42019
Iram Ismail <iram.8859@gmail.com>
2020-07-14 11:22:23 +0530

    CRUD Changes

---
+++ src/app/user-list/user-list.component.ts
@@ -0,0 +1,174 @@
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef, ChangeDetectorRef, HostListener, Input, Output } from '@angular/core';
+import { MdbTableDirective, MdbTablePaginationComponent } from 'angular-bootstrap-md';
+import { TemperatureServiceService } from '../services/temperature-service.service';
+import { TempService } from '../temp.service';
+import { NgForm } from '@angular/forms';
+import { formatDate } from '@angular/common';
+import * as FileSaver from 'file-saver';
+import * as XLSX from 'xlsx';
+import { UserService } from '../user.service';
+import { Router } from '@angular/router';
+import { AddRecordComponent } from '../add-record/add-record.component';
+import { EventEmitter } from 'protractor';
+@Component({
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.scss']
+  
+})
+export class UserListComponent implements OnInit {
+  @ViewChild(MdbTablePaginationComponent, { static: true }) mdbTablePagination: MdbTablePaginationComponent;
+  @ViewChild(MdbTableDirective, { static: true }) mdbTable:MdbTableDirective;
+  @ViewChild('myForm') myForm:NgForm;
+
+   EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+   EXCEL_EXTENSION = '.xlsx';
+
+  elements:any=[];
+  previous: any = [];
+  searchText: string = ''; 
+  search:any={}; 
+  startDate:any;
+  endDate:any;
+  headElements = ['Employee Name', 'Designation', 'Company Name', 'Email', 'Action'];
+  
+  message:string
+  @HostListener('input') oninput() 
+  { 
+    this.searchItems();
+  } 
+
+  constructor(private cdRef: ChangeDetectorRef, private ts:TemperatureServiceService,private myService:TempService,private userService:UserService,
+    private route:Router ) { }
+
+  ngOnInit() {
+  
+    this.userService.getuserList().subscribe((response:any)=>{
+      this.elements= response.body;
+      this.mdbTable.setDataSource(this.elements);
+      this.previous = this.mdbTable.getDataSource();
+      });
+      this.message="";
+     
+  }
+
+  onSubmitForm(){
+    this.message="";
+    this.search.startDate=this.startDate;
+    this.search.endDate=this.endDate;
+     this.myService.fetchTemperaturesOnSearch(this.search).subscribe((response:any)=>{
+      this.elements= response.body;
+      this.mdbTable.setDataSource(this.elements);
+      this.previous = this.mdbTable.getDataSource();
+     })
+  }
+
+  setStartDate(dt) {
+    const format = 'dd-MM-yyyy';
+    const myDate = dt.value;
+    const locale = 'en-US';
+     this.startDate = formatDate(myDate, format, locale);
+  }
+
+  setEndDate(dt) {
+    const format = 'dd-MM-yyyy';
+    const myDate = dt.value;
+    const locale = 'en-US';
+     this.endDate = formatDate(myDate, format, locale);
+  }
+
+  ngAfterViewInit() {
+    this.message="";
+    this.mdbTablePagination.setMaxVisibleItemsNumberTo(10);
+
+    this.mdbTablePagination.calculateLastItemIndex();
+    this.mdbTablePagination.calculateLastItemIndex();
+    this.cdRef.detectChanges();
+  }
+
+  searchItems() { 
+    this.message="";
+    const prev = this.mdbTable.getDataSource(); 
+    if (!this.searchText) {
+  this.mdbTable.setDataSource(this.previous); 
+  this.elements =this.mdbTable.getDataSource(); 
+  } 
+  if (this.searchText) { 
+    this.elements = this.mdbTable.searchLocalDataBy(this.searchText);
+  this.mdbTable.setDataSource(prev); 
+  } 
+  }
+  excel() {
+     const name='Temperature_Data from '+this.startDate+" to "+this.endDate;
+     const excelData:any=[];
+     this.elements.forEach(element => {
+       let obj = {
+        'Employee Name':element.appUser?.name,
+        'Shift':element.shift,
+        'Temperature Reading':element.reading?element.reading:element.noReading,
+        'Date':element.date
+       }
+      excelData.push(obj);
+     }); 
+     this.exportAsExcelFile(excelData,name)
+  }
+ 
+  
+  public exportAsExcelFile(json: any[], excelFileName: string): void {
+    
+    const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(json);
+    console.log('worksheet',worksheet);
+    const workbook: XLSX.WorkBook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
+    const excelBuffer: any = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
+    //const excelBuffer: any = XLSX.write(workbook, { bookType: 'xlsx', type: 'buffer' });
+    this.saveAsExcelFile(excelBuffer, excelFileName);
+  }
+
+  private saveAsExcelFile(buffer: any, fileName: string): void {
+    const data: Blob = new Blob([buffer], {
+      type: this.EXCEL_TYPE
+    });
+    FileSaver.saveAs(data, fileName + '_export_' + new Date().getTime() + this.EXCEL_EXTENSION);
+  }
+  editRow(el) {
+   
+   // console.log(el);
+    
+     this.route.navigate(["/edit",el.id]);
+    /* const elementIndex = this.elements.findIndex((elem: any) => el === elem);
+    const modalOptions = {
+      data: {
+        editableRow: el
+      }
+    };
+    this.modalRef = this.modalService.show(ModalEditComponent, modalOptions);
+    this.modalRef.content.saveButtonClicked.subscribe((newElement: any) => {
+        this.elements[elementIndex] = newElement;
+      });
+      this.mdbTable.setDataSource(this.elements);
+    } */
+  }
+  removeRow(el) {
+    this.userService.remove(el.id).subscribe((response:any)=>{
+      if(response.status==200){
+      this.message="success";
+      }
+      else
+      this.message="fail";
+     });
+     const elementIndex = this.elements.findIndex((elem: any) => el === elem);
+     this.mdbTable.removeRow(elementIndex);
+     this.mdbTable.getDataSource().forEach((el: any, index: any) => {
+         el.id = (index + 1).toString();
+       });
+       this.mdbTable.setDataSource(this.elements); console.log(el);
+    this.userService.remove(el.id).subscribe((response:any)=>{
+      if(response.status==200){
+      this.message="success";
+      }
+      else
+      this.message="fail";
+     });
+    }
+  
+}
---
+++ src/app/user.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
+import {map, catchError} from 'rxjs/operators';
+import { Observable, Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  baseURL = 'http://192.168.21.171:8080/';
+// baseURL = 'http://localhost:8082/';
+ temp:any=[];
+ 
+  constructor(private http:HttpClient) { }
+     getuserList(){
+      const url=this.baseURL+"users";
+      return this.http.get(url,{'observe':'response'}).pipe(map((response)=>{    
+      return response;
+    }));
+    }
+    remove(id){
+      const url=this.baseURL+"user/delete/"+id;
+      return this.http.get(url,{'observe':'response'}).pipe(map((response)=>{  
+     
+      return response;
+      }));
+    }
+    edit(id){
+
+      const url=this.baseURL+"user/"+id;
+      return this.http.get(url,{'observe':'response'}).pipe(map((response)=>{  
+        this.temp=response.body;
+        console.log(this.temp+"jjjjjjj");
+        return response;      
+      }));  
+    }
+
+
+      updateUser(data){
+        const url=this.baseURL+"/update";
+        return this.http.put(url,data,{'observe':'response'}).pipe(map((response:any)=>{
+        console.log(response.status+"updateddddd");
+        return response;
+         }));      
+        
+    }
+
+}
